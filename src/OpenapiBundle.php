4d4e68fc7fa14b2c016505c0554f60f59c2cbbbc
Niels Nijens <nijens.niels@gmail.com>
2018-06-02 18:19:08 +0200

    Add bundle class

---
+++ src/OpenapiBundle.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of the OpenapiBundle package.
+ *
+ * (c) Niels Nijens <nijens.niels@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Nijens\OpenapiBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * OpenapiBundle.
+ *
+ * @author Niels Nijens <nijens.niels@gmail.com>
+ */
+class OpenapiBundle extends Bundle
+{
+}
