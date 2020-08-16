28c0ea76a1ba4ef34f7b79d8ad60e0828d64d7a8
Niels Nijens <nijens.niels@gmail.com>
2020-08-16 19:26:13 +0200

    Add minimal Symfony application for testing

---
+++ tests/Functional/App/Kernel.php
@@ -0,0 +1,96 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the OpenapiBundle package.
+ *
+ * (c) Niels Nijens <nijens.niels@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Nijens\OpenapiBundle\Tests\Functional\App;
+
+use Nijens\OpenapiBundle\NijensOpenapiBundle;
+use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
+use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+use Symfony\Component\Routing\RouteCollectionBuilder;
+
+/**
+ * Micro Symfony application for functional testing of the OpenAPI bundle.
+ *
+ * @author Niels Nijens <nijens.niels@gmail.com>
+ */
+class Kernel extends BaseKernel
+{
+    use MicroKernelTrait;
+
+    /**
+     * File extensions used to load configuration files.
+     */
+    private const CONFIG_EXTS = '.{php,xml,yaml,yml}';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getCacheDir()
+    {
+        return $this->getTemporaryDirectory().'/var/cache/'.$this->environment;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getLogDir()
+    {
+        return $this->getTemporaryDirectory().'/var/log';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getProjectDir()
+    {
+        return __DIR__;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function registerBundles()
+    {
+        return [
+            new FrameworkBundle(),
+            new NijensOpenapiBundle(),
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configureContainer(ContainerBuilder $container, LoaderInterface $loader)
+    {
+        $loader->load(__DIR__.'/config.yaml');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configureRoutes(RouteCollectionBuilder $routes)
+    {
+        $routes->import(__DIR__.'/routing.yaml');
+    }
+
+    /**
+     * Returns a temporary directory to store the log and cache files.
+     */
+    private function getTemporaryDirectory(): string
+    {
+        return sys_get_temp_dir().'/OpenapiBundle';
+    }
+}
