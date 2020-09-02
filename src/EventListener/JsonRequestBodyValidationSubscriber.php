18e248fadbee78eed56b16a3e3d04ec0c682c6bb
Kiet <mail@kiettran.nl>
2020-09-02 17:11:08 +0200

    style: runned php-cs-fixer.

--- src/EventListener/JsonRequestBodyValidationSubscriber.php
+++ src/EventListener/JsonRequestBodyValidationSubscriber.php
@@ -18,7 +18,6 @@ use Nijens\OpenapiBundle\Json\JsonPointer;
 use Nijens\OpenapiBundle\Json\SchemaLoaderInterface;
 use Seld\JsonLint\JsonParser;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 
 /**
