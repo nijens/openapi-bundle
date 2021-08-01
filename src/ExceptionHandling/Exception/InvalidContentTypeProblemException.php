dcbaa025876ed150fc879443d245493950b96cd0
Niels Nijens <nijens.niels@gmail.com>
2021-08-01 22:59:34 +0200

    Add extended problem exceptions

---
+++ src/ExceptionHandling/Exception/InvalidContentTypeProblemException.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the OpenapiBundle package.
+ *
+ * (c) Niels Nijens <nijens.niels@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Nijens\OpenapiBundle\ExceptionHandling\Exception;
+
+final class InvalidContentTypeProblemException extends ProblemException
+{
+}
