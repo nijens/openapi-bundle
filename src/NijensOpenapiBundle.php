f4cfa958343476d84d5073db4de308020e61cdf1
Niels Nijens <nn@ehvg.nl>
2023-10-27 14:18:58 +0200

    Move NijensOpenapiExtension::getSymfonyVersion to NijensOpenapiBundle

--- src/NijensOpenapiBundle.php
+++ src/NijensOpenapiBundle.php
@@ -11,8 +11,10 @@
 
 namespace Nijens\OpenapiBundle;
 
+use Symfony\Component\Config\Loader\LoaderInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\Serializer\DependencyInjection\SerializerPass;
 
 /**
@@ -29,5 +31,21 @@ class NijensOpenapiBundle extends Bundle
         $container->addCompilerPass(
             new SerializerPass('nijens_openapi.serializer', 'nijens_openapi.serializer.normalizer')
         );
+    }
+
+    public static function getSymfonyVersion(): int
+    {
+        $kernel = new class('symfony_version', false) extends Kernel {
+            public function registerBundles(): iterable
+            {
+                return [];
+            }
+
+            public function registerContainerConfiguration(LoaderInterface $loader): void
+            {
+            }
+        };
+
+        return $kernel::VERSION_ID;
     }
 }
