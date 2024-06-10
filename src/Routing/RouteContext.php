7f598ba30ed09bdead08a7f00049bebad94111e4
Niels Nijens <nn@ehvg.nl>
2024-06-10 16:06:44 +0200

    Add loading of validateBeforeFirewall property in x-openapi-bundle schema extension to RouteLoader

--- src/Routing/RouteContext.php
+++ src/Routing/RouteContext.php
@@ -34,6 +34,8 @@ final class RouteContext
 
     public const REQUEST_VALIDATE_QUERY_PARAMETERS = 'request_validate_query_parameters';
 
+    public const REQUEST_VALIDATE_BEFORE_FIREWALL = 'request_validate_before_firewall';
+
     public const DESERIALIZATION_OBJECT = 'deserialization_object';
 
     public const DESERIALIZATION_OBJECT_ARGUMENT_NAME = 'deserialization_object_argument_name';
